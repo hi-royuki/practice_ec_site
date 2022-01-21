e3d03a31089e61c539ebcf1d5b546464685d26ba
EC2 Default User <ec2-user@ip-172-31-16-149.ap-northeast-1.compute.internal>
2022-01-21 06:43:32 +0000

    [Update]バリデーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,16 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+
+  validates :post_code, presence: true
+  validates :prefecture_code, presence: true
+  validates :city_code, presence: true
+  validates :street, presence: true
+  validates :telephone_number, presence: true
+
 end
