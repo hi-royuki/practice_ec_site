837d49cfce27a280d6f0782b5666db021f0103d2
EC2 Default User <ec2-user@ip-172-31-16-149.ap-northeast-1.compute.internal>
2022-01-23 02:27:40 +0000

    [Update]会員側ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
 
 # ーーーーーー会員側のパス設定ーーーーー
-  # skipオプションで対応付けをスキップする
-  devise_for :customers, skip: [:registrations, :sessions, :passwords]
-  # skipしたままだと利用できない。devise_scopeメソッドでパスを任意に指定する
-  devise_scope :customer do
-    get '/customers/sign_up', to: 'public/customers/registrations#new' # 新規登録
-    post '/customers', to: 'public/customers/registrations#create'
-    get '/customers/sign_in', to: 'public/customers/sessions#new' # ログイン
-    post '/customers/sign_in', to: 'public/customers/sessions#create'
-    delete '/customers/sign_out', to: 'piblic/customers/sessions#destroy' # ログアウト
+  devise_for :customers, skip: [:registrations, :sessions, :passwords] # skipオプションで対応付けをスキップする
+  devise_scope :customer do # skipしたままだと利用できない。devise_scopeメソッドでパスを任意に指定する
+    get 'customers/sign_up', to: 'public/customers/registrations#new', as: :new_customer_registration # 新規登録
+    post '/customers', to: 'public/customers/registrations#create', as: :customer_registration
+    get '/customers/sign_in', to: 'public/customers/sessions#new', as: :new_customer_session # ログイン
+    post '/customers/sign_in', to: 'public/customers/sessions#create', as: :customer_session
+    delete '/customers/sign_out', to: 'piblic/customers/sessions#destroy', as: :destroy_customer_session # ログアウト
   end
 # ーーーーーーここまでーーーーーー
 
+
+
 # ーーーーーー管理者側のパス設定ーーーーー
   devise_for :admins
-  
-# ーーーーーーここまでーーーーーー  
+
+# ーーーーーーここまでーーーーーー
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
