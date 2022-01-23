82ec69a19229c9ff6db3a8af47e9d67d57094375
EC2 Default User <ec2-user@ip-172-31-16-149.ap-northeast-1.compute.internal>
2022-01-23 15:30:49 +0000

    [OK]都道府県自動入力

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,32 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery <--追加
-//= require jquery_ujs <--追加
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery.jpostal <--追加
+//= require jquery.jpostal
 //= require_tree .
+
+$(function() {
+  $(document).on('turbolinks:load', () => {
+    $('#customer_post_code').jpostal({
+      postcode : [
+        '#customer_post_code'
+      ],
+      address: {
+        "#customer_prefecture_code": "%3", // # 都道府県が入力される
+        "#customer_city_code"           : "%4%5", // # 市区町村と町域が入力される
+        "#customer_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+      }
+    });
+  });
+});
+
+
+// # 入力項目フォーマット
+// #   %3  都道府県
+// #   %4  市区町村
+// #   %5  町域
+// #   %6  大口事業所の番地 ex)100-6080
+// #   %7  大口事業所の名称
