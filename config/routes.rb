379047465093c5d5f69d6b172fa1463ee84716a6
EC2 Default User <ec2-user@ip-172-31-16-149.ap-northeast-1.compute.internal>
2022-01-23 02:00:25 +0000

    [Update]customerのパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :customers
+
+
+# ーーーーーー会員側のパス設定ーーーー
+  # skipオプションで対応付けをスキップする
+  devise_for :customers, skip: [:registrations, :sessions, :passwords]
+  # skipしたままだと利用できない。devise_scopeメソッドでパスを任意に指定する
+  devise_scope :customer do
+    get '/customers/sign_up', to: 'public/customers/registrations#new'
+    post '/customers', to: 'public/customers/registrations#create'
+
+    get '/customers/sign_in', to: 'public/customers/sessions#new'
+    post '/customers/sign_in', to: 'public/customers/sessions#create'
+    delete '/customers/sign_out', to: 'piblic/customers/sessions#destroy'
+  end
+# ーーーーーーここまでーーーーーー
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
