5483cc0ba3ba44a2b781d6e66cfcbd87d19301d4
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-02-04 11:00:38 +0000

    [Update]item routes変更

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -27,7 +27,7 @@ class Admins::SessionsController < Devise::SessionsController
   # ログイン後及びログアウト後のパス設定
   protected
   def after_sign_in_path_for(resource)
-    admin_items_path
+    admins_items_path
   end
 
   def after_sign_out_path_for(resource)
