fa95b1273474f31218153591079be991df190e2e
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-02 14:37:48 +0000

    [Update]商品追加ページ作成

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,10 +1,19 @@
 class Admins::ItemsController < ApplicationController
+
 before_action :authenticate_admin!,only: [:create,:edit,:update,:index, :show, :new]
 
-  def new 
+  def new
+    @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redilrect_to admins_items_path
+  end
+
   def index
+    @items = Item.all
   end
 
   def show
@@ -12,9 +21,9 @@ before_action :authenticate_admin!,only: [:create,:edit,:update,:index, :show, :
 
   def edit
   end
-  
+
   def update
   end
-  
-  
+
+
 end
