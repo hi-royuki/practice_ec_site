5318d4935619a8aab0e8691387b06bd65a248f32
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-02 15:03:48 +0000

    [Update]Runtime Errorが表示されたため記述

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+# item商品追加時のエラーのため記述
+Refile.secret_key = 'dab15c0ebd5d4abb9b609ea3c048713a1a33b6c051998cd2dec4d99eaa9eac813208dd0c9e466a5cd8e7ef6905ae94478002aa844deca0acf8dc8150a447e7b5'
