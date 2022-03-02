8d9734516d84447966578470cad7144a48f5ee2a
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-02 14:55:10 +0000

    [Update]商品追加のバリデーションセッってい

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+
+# 画像投稿
+  attachment :image
+
+# バリデーション
+  validates :item_name, presence: true
+  validates :image, presence: true
+  validates :introduction, presence: true
+  validates :no_tax_price, presence: true
+
 end
