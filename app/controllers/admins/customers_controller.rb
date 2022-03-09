84b3383599fa5255c19bd932b3cf74993bf9431f
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-03-09 11:19:15 +0000

    [Update]会員詳細画面作成

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,11 +1,12 @@
 class Admins::CustomersController < ApplicationController
 
   def index
-     
+
     @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
@@ -13,4 +14,11 @@ class Admins::CustomersController < ApplicationController
 
   def update
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :prefecture_code, :city_code, :street, :other_address, :telephone_number, :is_active)
+  end
+
 end
